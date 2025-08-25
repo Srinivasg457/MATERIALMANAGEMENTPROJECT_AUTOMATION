06dd2a4c93508140b56f2b830980b4cea0e1f102
Srinivasg457 <srinivasg457@gmail.com>
2025-08-25 14:09:56 +0530

    features

--- src/test/java/testRunner/TestRun.java
+++ src/test/java/testRunner/TestRun.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/java/Features/1_materialManagementCustomer.feature",
+        features = "src/test/java/Features",
 
         //"src/test/java/Features/1_materialManagementCustomer.feature",
         //src/test/java/Features/workroomlogin.feature
